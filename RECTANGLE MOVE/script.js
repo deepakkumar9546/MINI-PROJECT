3bed2754cf7becde0cc71b0dcf3092b117c8afc8
Deepak Kumar <137646137+deepakkumar9546@users.noreply.github.com>
2024-07-22 22:17:24 +0530

    Update script.js

--- RECTANGLE MOVE/script.js
+++ RECTANGLE MOVE/script.js
@@ -3,7 +3,7 @@ window.addEventListener("mousemove", function(details){
     var rect=document.querySelector("#rect");
     var xval=gsap.utils.mapRange(0,window.innerWidth,100+rect.getBoundingClientRect().width/2,window.innerWidth-(100+rect.getBoundingClientRect().width/2),details.clientX);
 
-    gsap.to("#rect", {
+    gsap.to("#rect",{
         left: xval,
         ease: Power3,
     })
